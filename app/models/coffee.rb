59c4e0479a60eb5a39f2365844a190d38a3488ce
victor guilherme <victorg.queiroz@hotmail.com>
2023-05-13 15:06:55 -0300

    ta saindo mano

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,3 +1,11 @@
 class Coffee < ApplicationRecord
   belongs_to :user
+
+  def fetch_random_coffee_image
+    response = RestClient.get 'https://api.unsplash.com/photos/random?query=coffee', {params: {client_id: OC47tUJ8sTjie4ghNrAo03aUPDi_OCxB8FP1rPaohFI}}
+
+    data = JSON.parse(response.body)
+    data["urls"]["small"]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :coffees
   root to: "coffees#index"
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
