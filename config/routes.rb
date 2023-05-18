3f8a24c9e60350263e26357009e75f57ba3510b1
victor guilherme <victorg.queiroz@hotmail.com>
2023-05-18 19:43:12 -0300

    em andamento

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :coffees
+  resources :devise
   # get "market", to: "market#show"
   resources :markets
   root to: "coffees#index"
+  get "edit", to: "coffees#update"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
