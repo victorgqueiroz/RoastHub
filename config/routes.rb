def4f048582ccfbae3b43e38325d03cfe735e877
victor guilherme <victorg.queiroz@hotmail.com>
2023-05-18 21:48:07 -0300

    pull request

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
   resources :markets
   root to: "coffees#index"
   get "edit", to: "coffees#update"
+
+  resources :coffees do
+    collection do
+      get :my
+    end
+  end
+  root to: "coffees#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
