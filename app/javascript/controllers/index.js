a26615950bee6bb24362d5456f966ad58cc29340
victor guilherme <victorg.queiroz@hotmail.com>
2023-05-20 16:11:33 -0300

    front end

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,7 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+$(window).scroll(function() {
+  $('.button').css('top', $(this).scrollTop());
+});
