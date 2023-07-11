46d01bfb3496b14e71e588fab747fca7467c6de2
victor guilherme <victorg.queiroz@hotmail.com>
2023-07-10 23:01:47 -0300

    vers 1.0 imgs random

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,5 @@
+Unsplash.configure do |config|
+  config.application_access_key = "vEvxArfpZPAxKiJIn18VquSXK7npyAl-gN8iHdcDYCU"
+  config.application_secret = "nvlsuCAnJXZHxngRxyuaZaIeb5CefA2kAT6jk34jR1s"
+  config.application_redirect_uri = "https://unsplash.com/oauth/applications/473336"
+end
